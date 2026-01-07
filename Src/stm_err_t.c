2c4330ebb5d69adaddbc3f8f98c4f02de6ed0086
ziadAhmed19 <ziadwizard612@gmail.com>
2026-01-07 13:44:51 +0200

    Added the code logic for the stm_err_to_name function UART logging is defined at the moment once library is constructed functionality will be compeleted will rasie a github issue to note that in the future

--- Src/stm_err_t.c
+++ Src/stm_err_t.c
@@ -5,4 +5,20 @@
  *      Author: ziad-ahmed
  */
 
+#include "stm_err_t.h"
 
+stm_err_to_name(stm_err_t ERROR_CODE){
+	switch(ERROR_CODE){
+
+		case STM_ERR_INVALID_ARG:	break;
+		case STM_ERR_INVALID_SIZE:	break;
+		case STM_ERR_INVALID_STATE:	break;
+		case STM_ERR_NOT_FOUND:		break;
+		case STM_ERR_TIMEOUT:		break;
+		case STM_ERR_NOT_FINISHED:	break;
+
+		default: break;
+
+
+	}
+}
